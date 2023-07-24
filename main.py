e2506e8d2de630e63a40623397c95a12fb318314
Mate Chocheli <matechocheli2@gmail.com>
2023-07-24 12:01:24 +0400

    restart making the program

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
 
-from sys import *
-from pmmparser import parse
-from pmmInterpreter import Interpret
-
-if __name__ == '__main__':
-    Interpret(parse(argv[1]))
     
--- pmmparser.py
+++
@@ -1,9 +0,0 @@
-from colorama import Fore
-from pmmlexer import lexer
-
-def parse(file):
-    print(Fore.YELLOW + 'parsing started')
-    contents = open(file, 'r').read()
-    print(Fore.GREEN + 'parsing finished\n')
-    tokens = lexer(contents)
-    return tokens
